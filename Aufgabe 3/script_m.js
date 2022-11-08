b8d8bc8c87a7fccd509ff7d9520af772a3902a23
Matthias Roy <mar0493@thi.de>
2022-11-08 15:45:03 +0100

    Klick auf Dokument schließt sug-friends - added

--- Aufgabe 3/script_m.js
+++ Aufgabe 3/script_m.js
@@ -12,10 +12,10 @@ function getUsers() {
             if (data.length > 0) {
                 var input = document.getElementById("input-friends");
 
-                input.addEventListener("input", function(e) {
+                input.addEventListener("input", function (e) {
                     var list, item, value = this.value; //this.value = Inhalt von Textbox (in diesem Kontext)
 
-                    clearSuggestList(input);
+                    clearSuggestList();
 
                     list = document.createElement("div");
                     list.setAttribute("id", "sug-friends-items");
@@ -23,15 +23,15 @@ function getUsers() {
                     this.parentNode.appendChild(list); //Fügt das DIV dem DIV über Input hinzu.
 
                     data.forEach(element => {
-                        if(element.substr(0, value.length).toUpperCase() == value.toUpperCase()){
+                        if (element.substr(0, value.length).toUpperCase() == value.toUpperCase()) {
                             item = document.createElement("div");
                             item.innerHTML = "<strong>" + element.substr(0, value.length) + "</strong>";
                             item.innerHTML += element.substr(value.length);
                             item.innerHTML += "<input type='hidden' value='" + element + "'>";
 
-                            item.addEventListener("click", function(e) {
+                            item.addEventListener("click", function (e) {
                                 input.value = this.getElementsByTagName("input")[0].value;
-                                clearSuggestList(input);
+                                clearSuggestList();
                             });
                             list.appendChild(item);
                         }
@@ -52,7 +52,9 @@ function getUsers() {
 function clearSuggestList(input) {
     var elements = document.getElementsByClassName("sug-friends-items");
     for (var i = 0; i < elements.length; i++) {
-        elements[i].parentNode.removeChild(elements[i]);
+        if (input != elements[i] && input != document.getElementById("input-friends")) {
+            elements[i].parentNode.removeChild(elements[i]);
+        }
     }
 }
 
@@ -60,7 +62,7 @@ function checkUserExist() {
     var value = document.getElementById("input-friends").value;
     var submitForm = document.getElementById("submitFormAddFriend");
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4) {
             if (xmlhttp.status == 204) {
                 console.log("Existiert");
@@ -75,3 +77,7 @@ function checkUserExist() {
     xmlhttp.open("GET", window.chatServer + "/" + window.chatCollectionId + "/user/" + value);
     xmlhttp.send();
 }
+
+document.addEventListener("click", function (e) {
+    clearSuggestList(e.target);
+});
